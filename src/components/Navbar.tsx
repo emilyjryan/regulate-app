23f7254db86cf1ac9d9ccbbd77c47ac9a8aae797
emilyjryan <emilyryanmac@gmail.com>
2023-02-16 23:24:02 -0800

    trying and failing to get the navbar burger working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar () {
+
+
+  // document.addEventListener('DOMContentLoaded', () => {
+
+  //   // Get all "navbar-burger" elements
+  //   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+  //   // Check if there are any navbar burgers
+  //   if ($navbarBurgers.length > 0) {
+  
+  //     // Add a click event on each of them
+  //     $navbarBurgers.forEach( el => {
+  //       el.addEventListener('click', () => {
+  
+  //         // Get the target from the "data-target" attribute
+  //         const target = el.dataset.target;
+  //         const $target = document.getElementById(target);
+  
+  //         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  //         el.classList.toggle('is-active');
+  //         $target.classList.toggle('is-active');
+  
+  //       });
+  //     });
+  //   }
+  
+  // });
+
     return (
         <>
 
@@ -12,6 +40,11 @@ export default function Navbar () {
             <a className="navbar-item" href="/">
                 <h2 style={{fontSize: '40px'}}>Regulate</h2>
             </a>
+            {/* <a role="button" className="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a> */}
         </div>
 
         <div id="options" className="navbar-menu">
