ecf14c401b18526940a5bfea33b604abe9e66483
emilyjryan <emilyryanmac@gmail.com>
2023-02-28 09:36:31 -0800

    fixed portfolio link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,14 @@ export default function Footer() {
                     <div className="is-flex is-justify-content-space-around is-align-items-center" style={{color: '#c0d4f8', paddingLeft: '18%', paddingRight: '18%'}}>
                         <div className="is-flex-direction-column text-center">
                             <p><strong style={{fontSize: '28px'}}>Regulate</strong></p>
-                            <a href="https://nextjs-portfolio-92yvlktgj-emilyjryan.vercel.app/">by Emily Ryan</a>
+                            <a href="https://emily-j-ryan.vercel.app/">by Emily Ryan</a>
                         </div>
                   
                         <a className="" href="https://www.linkedin.com/in/emilyjudithryan/">LinkedIn</a>
             
                         <a className="" href="https://github.com/emilyjryan">GitHub</a>
                       
-                        <a className="" href="https://nextjs-portfolio-92yvlktgj-emilyjryan.vercel.app/">Portfolio</a>
+                        <a className="" href="https://emily-j-ryan.vercel.app/">Portfolio</a>
 
                     </div>
                 </div>
